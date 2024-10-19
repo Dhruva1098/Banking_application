89b52e5a5466d7ec9f84f302786d7a20d2326670
Dhruva Sharma <dhruva1098@gmail.com>
2024-10-19 20:13:02 +0530

    added password hashing and authenticateing login

--- customer_handler.c
+++ customer_handler.c
@@ -2,6 +2,7 @@
 #include "headers/customer_handler.h"
 
 #include <stdio.h>
+#include <sys/fcntl.h>
 #include <unistd.h>
 #include <string.h>
 #include <stdlib.h>
@@ -11,36 +12,49 @@
 #define DB_PATH "database/customer_db"
 #define BUFFER_SIZE 1024
 
+unsigned long hashed_password(const char *password) {
+  unsigned long hash = 5381;
+  int c;
+  while ((c = *password++)) {
+    hash = ((hash << 5) + hash) + c;
+  }
+  return hash;
+}
+void lock_customer(int fd, int account_id) {
+  struct flock lock = {0};
+  lock.l_type = F_WRLCK;
+  lock.l_whence = SEEK_SET;
+  lock.l_start = sizeof(customerRecord) * account_id;
+  lock.l_len = sizeof(customerRecord);
+  fcntl(fd, F_SETLKW, &lock);
+}
+void unlock_customer(int fd, int account_id) {
+  struct flock lock = {0};
+  lock.l_type = F_UNLCK;
+  lock.l_whence = SEEK_SET;
+  lock.l_start = sizeof(customerRecord) * account_id;
+  lock.l_len = sizeof(customerRecord);
+  fcntl(fd, F_SETLK, &lock);
+}
 int fetch_account_balance(int account_id, int* balance) {  // helper
-  FILE *file = fopen(DB_PATH, "r");
-  if (file == NULL) {
-    perror("Error opening customer database");
-    return -1;
-  }
-  int fd = fileno(file);
-  if (flock(fd, LOCK_SH) != 0) {
-    perror("error locking the file for reading");
-    fclose(file);
-    return -1;
-  }
-  char line[BUFFER_SIZE];
-  while (fgets(line, sizeof(line), file)) {
-    int file_account_id;
-    int file_account_balance;
-    sscanf(line, "%d, %d", &file_account_id, &file_account_balance);
-    if (file_account_id == account_id) {
-      *balance = file_account_balance;
-      flock(fd, LOCK_UN);
-      fclose(file);
-      return 0;
-    }
-  }
-  flock(fd, LOCK_UN);
-  fclose(file);
-  return -1;  // account not found
+  int fd = open(DB_PATH, O_RDONLY);
+  if (fd < 0) { return -1; }
+  customerRecord record;
+  lseek(fd, sizeof(customerRecord) * account_id, SEEK_SET);
+  read(fd, &record, sizeof(customerRecord));
+  close(fd);
+  *balance = record.balance;
+  return 0;
 }
 
-void view_account_balance(int connfd) {
-  char account_id_buff[BUFFER_SIZE] = {0};
-  char request[] = "please enter your account ID:\n";
-
+void view_account_balance(int connfd, int account_id) {
+  int balance;
+  if (fetch_account_balance(account_id, &balance) == 0) {
+    char response[100];
+    snprintf(response, sizeof(response), "Your balance is: %d\n", balance);
+    write(connfd, response, strlen(response));
+  } else {
+    char response[] = "Unable to fetch your balance \n";
+    write(connfd, response, strlen(response));
+  }
+}
--- headers/customer_handler.h
+++ headers/customer_handler.h
@@ -1,8 +1,22 @@
 #ifndef SS_PROJECT_HEADERS_CUSTOMER_HANDLER_H_
 #define SS_PROJECT_HEADERS_CUSTOMER_HANDLER_H_
 
+#include <stdint.h>
+
+typedef struct {
+  int account_id;
+  char name[50];
+  float balance;
+  char password_hash[65];
+} customerRecord;
+
+int login_customer(int connfd, int* account_id);
+int auth_customer(int connfd, int* account_id);
+void view_account_balance(int connfd, int account_id);
 int fetch_account_balance(int account_id, int* balance);
-void view_account_balance(int connfd);
+void lock_customer(int fd, int account_id);
+void unlock_customer(int fd, int account_id);
+unsigned long hash_password(const char* password);
 void deposit_money(int connfd);
 void withdraw_money(int connfd);
 void transfer_funds(int connfd);
